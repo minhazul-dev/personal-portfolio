0b8e0633fbb16eabcaa82450ed69bb4cb11e1755
minhazul-dev <minhazul.dev@gmail.com>
2021-05-05 20:47:02 +0600

    updating

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import blog1 from "../../images/blog1.png"
 import Zoom from 'react-reveal/Zoom';
+import blogCovered from "../../images/blog cover.png"
 
 const Blogs = () => {
     return (
@@ -12,11 +13,13 @@ const Blogs = () => {
             <div className="row">
                 <div className="col-md-6 mt-5">
                     <div className="card mb-3">
-                        <img src={blog1} className="card-img-top" alt="..." />
+                        <img src={blogCovered} className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Title of the blog</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <button type="button" className="btn btn-info">Read in medium</button>
+                            <h5 className="card-title">Some fundamentals of Javascript</h5>
+                            <p className="card-text">In this blog i have tried to explain some basic fundamentals of Javascript.</p>
+                            <a target="_blank" href="https://minhazul-dev.medium.com/some-fundamentals-of-javascript-780b25e23b64">
+                           <button type="button" className="btn btn-info">Read in medium</button>
+                           </a>
                         </div>
                     </div>
 
@@ -27,7 +30,12 @@ const Blogs = () => {
                         <div className="card-body">
                             <h5 className="card-title">title of the blog</h5>
                             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            {/* <Link to="https://minhazul-dev.medium.com/some-fundamentals-of-javascript-780b25e23b64">
                             <button type="button" className="btn btn-info">Read in medium</button>
+                            </Link> */}
+                           <a target="_blank" href="https://minhazul-dev.medium.com/some-fundamentals-of-javascript-780b25e23b64">
+                           <button type="button" className="btn btn-info">Read in medium</button>
+                           </a>
                         </div>
                     </div>
                 </div>
